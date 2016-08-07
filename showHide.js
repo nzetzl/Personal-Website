379d5e7ae421f1f5c235cc99afc195bf9cab93b8
nzetzl <nzetzl@purdue.edu>
2016-08-07 00:19:45 -0400

    removed home tab. A home tab is unnecessary when you can just click on the title of the page. The title now links to the home page view

--- showHide.js
+++ showHide.js
@@ -1,37 +1,30 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 $(document).ready(function () {
-
-    $(".tabs").click(function () {
-        "use strict";
+    "use strict";
+    $(".tabs, h1").click(function () {
         var sectionID = $(this).attr("id");
-        if (sectionID != "resumeTab") {
+        if (sectionID !== "resumeTab") {
             $(".section").fadeOut();
         }
     });
-    
     $("#homeTab").click(function () {
-        "use strict";
         $("#home").delay(500).fadeIn();
     });
 
     $("#skillsTab").click(function () {
-        "use strict";
         $("#skills").delay(500).fadeIn();
     });
 
     $("#interestsTab").click(function () {
-        "use strict";
         $("#interests").delay(500).fadeIn();
     });
 
     $("#projectsTab").click(function () {
-        "use strict";
         $("#projects").delay(500).fadeIn();
     });
     
     $("#resumeTab").click(function () {
-        "use strict";
         window.open("/Resume.pdf", "_blank");
     });
 
