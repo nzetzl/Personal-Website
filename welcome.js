8aff6904f4d3b08ce2140b430db15e9d1b19e88b
nzetzl <nzetzl95@gmail.com>
2025-12-04 11:24:14 -0600

    updated skills section and welcome modal, and some overall clean up

--- welcome.js
+++ welcome.js
@@ -6,16 +6,15 @@ $(document).ready(function () {
     var modal = document.getElementById('welcomeBox');
     var span = document.getElementsByClassName("close")[0];
     var arrow = document.getElementById("modalArrow");
-    var description = document.getElementById("description");
 	var toSite = document.getElementById("skip");
 	var p = document.getElementById("welcome");
-	var numIntroItems = 3;
+	var numIntroItems = document.getElementById("welcomeItems").childElementCount;
 	var count = 0;
-        
+
     span.onclick = function () {
         modal.style.display = "none";
     };
-    
+
     arrow.onclick = function () {
 		count++;
 		if (count === numIntroItems - 1) {
@@ -24,23 +23,20 @@ $(document).ready(function () {
 		}
 		jQuery(p).fadeOut();
 		p = jQuery(p).next();
-		console.log(p);
 		p.delay(500).fadeIn();
-//        $("#welcome, #modalArrow").fadeOut();
-//        $("#description").delay(500).fadeIn();
     };
-	
+
 	toSite.onclick = function () {
 		modal.style.display = "none";
-	}
+	};
 
     window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
         }
-        
+
     };
-    
+
     window.onload = function () {
         var welcomeShown = sessionStorage.getItem("welcomeShown");
         if (welcomeShown === null || welcomeShown === false) {
